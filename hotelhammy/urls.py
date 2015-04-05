6e1fa3310fd5c54454e64fa04cb78b3c1119845c
Ng Zhi An <ngzhian@gmail.com>
2015-04-05 16:09:31 +0000

    updat eurl

--- hotelhammy/urls.py
+++ hotelhammy/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^listing/', include('listing.urls')),
+    url(r'^hotel/', include('hotel.urls')),
+    url(r'^listing/', include('listing.urls', namespace="listing")),
     url(r'^admin/', include(admin.site.urls)),
 ]
